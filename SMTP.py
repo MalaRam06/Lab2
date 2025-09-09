942dafdfe7eacbf60988ee25620ac0c3309f33fb
MalaRam06 <iitiansmalaram@gmail.com>
2025-09-09 22:24:20 +0530

    Update code

--- SMTP.py
+++ SMTP.py
@@ -2,10 +2,9 @@ import smtplib
 from email.mime.text import MIMEText
 
 def main():
-    sender = "iitiansmalaram@gmail.com"       # replace with your Gmail
-    # recipient = "dineshsaini00085@gmail.com"  # replace with a friend's email
-    recipient = "malaramameghavala118@gmail.com"
-    app_password = "tkvc ijbm bwid qggb"    # generate from Google account
+    sender = ""        # replace with your Gmail
+    recipient = ""     # replace with a friend's email
+    app_password = ""  # generate from Google account
 
     msg = MIMEText("Hello! This is a test email from Mala (SMTP Lab).")
     msg["Subject"] = "SMTP Lab Test"
