261ac8cbdd986b046ac4bf6c106c3ee2bb34619e
MalaRam06 <iitiansmalaram@gmail.com>
2025-09-13 15:36:22 +0530

    modified hii.py

--- hii.py
+++ hii.py
@@ -2,3 +2,15 @@
 # print(requests.__version__)
 import sys
 print(sys.executable)
+import requests
+print(requests.__version__)
+
+import requests
+print(requests.__file__)
+print(requests.__file__) #inside python to find the exact location of the requests module.
+import smtplib
+import ftplib
+import requests
+print(smtplib.__file__)
+print(ftplib.__file__)
+print(requests.__file__)
